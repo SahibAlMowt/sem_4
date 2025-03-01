f3129824d9e33ee2a120606e420f2f7512c03721
SahibAlMowt <sahibshukurlu2006@gmail.com>
2025-03-01 15:29:17 +0400

    01.03

--- 01.03___json/parser.cpp
+++ 01.03___json/parser.cpp
@@ -1,4 +1,5 @@
 #include "parser.h"
+#include "JSON.h"
 
 void JSONParser::parse()
 {
@@ -13,17 +14,53 @@ void JSONParser::parse()
             token = tokenizer.getToken();
             std::cout << token.toString() << "\n";
 
-            switch (token.type)
+            switch(token.type)
             {
                 case TOKEN::CURLY_OPEN:
-                    std::shared_ptr<JSON::JSONNode> parsedObject = parseObject();
-                    parsedObject -> printNode(0);
+                {
+                    std::shared_ptr<JSON_Node> parsedObject = parseObject();
+            //        parsedObject -> printNode(0);
                     if(!root)
                     {
                         root = parsedObject;
                     }
                     break;
-                case     
+                }
+                case TOKEN::ARRAY_OPEN:
+                {
+                    std::shared_ptr<JSON_Node> parsedList = parseList();
+                    if(!root)
+                    {
+                        root = parsedList;
+                    }
+                    break;
+                }
+                case TOKEN::STRING:
+                {
+                    tokenizer.rollBackToken();
+                    std::shared_ptr<JSON_Node> parsedString = parseString();
+                    if(!root)
+                    {
+                        root = parsedString;
+                    }
+                    break;
+                }
+                case TOKEN::NUMBER:
+                {
+                    tokenizer.rollBackToken();
+                    std::shared_ptr<JSON_Node> parsedNumber = parseNumber();
+                    if(!root)
+                    {
+                        root = parsedNumber;
+                    }
+                    break;
+                }
+                case TOKEN::BOOLEAN:
+                {
+                    tokenizer.rollBackToken();
+                    std::shared_ptr<JSON_Node> parsedBoolean = parseBoolean();
+                    break;
+                }
             }
         }
         catch(const std::logic_error &e)
