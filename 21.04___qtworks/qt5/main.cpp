6f514a6bc1ff21bec87828de417be068b10ef793
SahibAlMowt <sahibshukurlu2006@gmail.com>
2025-04-21 15:12:07 +0400

    21.04

--- 21.04___qtworks/qt5/main.cpp
+++ 21.04___qtworks/qt5/main.cpp
@@ -1,11 +1,11 @@
-#include "label.h"
+#include "slider.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Label w;
+    Slider w;
     w.show();
     return a.exec();
 }
--- 21.04___qtworks/qt5/slider.cpp
+++ 21.04___qtworks/qt5/slider.cpp
@@ -7,4 +7,12 @@ Slider::Slider(QWidget *parent): QWidget(parent)
     auto *hbox = new QHBoxLayout(this);
 
     slider = new QSlider(Qt::Horizontal, this);
+
+    hbox -> addWidget(slider);
+
+    label = new QLabel("0", this);
+
+    hbox -> addWidget(label);
+
+    connect(slider, &QSlider::valueChanged, label, qOverload<int>(&QLabel::setNum));
 }
